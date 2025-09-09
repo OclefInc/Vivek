c46617887f454a7d4170dc2595ae60819ad13574
MATTHEW BELL <matthew@oclef.com>
2025-09-09 13:33:49 -0400

    create comments forms and configure routes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user=current_user
+    @comment.save
+    if @comment.annotation_type=="Lesson"
+      @episode=@comment.annotation
+      @project=@episode.assignment
+      redirect_to project_episode_path(@project, @episode)
+    else
+      redirect_to root_path
+    end
+  end
+  private
+  def comment_params
+      params.expect(comment: [ :note, :annotation_id, :annotation_type ])
+    end
+end

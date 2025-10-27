9133fecbe38b91acc83dac71239c7336c05675e4
Phong Le <phong@oclef.com>
2025-10-27 08:10:53 -0700

    overlay next video countdown

--- app/javascript/controllers/video_autoplay_controller.js
+++ app/javascript/controllers/video_autoplay_controller.js
@@ -23,11 +23,31 @@ export default class extends Controller {
   }
 
   showCountdown() {
+    // Find the background div (parent of the video)
+    const backgroundDiv = this.element.parentElement
+
+    // Ensure the background div has relative positioning
+    if (window.getComputedStyle(backgroundDiv).position === 'static') {
+      backgroundDiv.style.position = 'relative'
+    }
+
     // Create countdown overlay
     this.countdownElement = document.createElement('div')
-    this.countdownElement.className = 'fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'
+    this.countdownElement.className = 'absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center'
+    // Add inline styles to ensure positioning works
+    this.countdownElement.style.cssText = `
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.75);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      z-index: 10;
+    `
     this.countdownElement.innerHTML = `
-      <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 z-10">
       <div class="text-center">
         <p class="text-white text-2xl mb-4">Next lesson starting in...</p>
         <p class="text-white text-6xl font-bold countdown-number">5</p>
@@ -35,15 +55,14 @@ export default class extends Controller {
           Cancel
         </button>
       </div>
-    </div>
     `
-    // mmove countdownElement to be a sibling of the video element
-    // this.element.parentNode.insertBefore(this.countdownElement, this.element.nextSibling)
+
     // Add cancel button event
     const cancelButton = this.countdownElement.querySelector('button')
     cancelButton.addEventListener('click', () => this.clearCountdown())
 
-    this.element.parentNode.parentNode.insertBefore(this.countdownElement, this.element.parentNode.parentNode.firstChild)
+    // Append to the background div to position it on top of the video
+    backgroundDiv.appendChild(this.countdownElement)
 
     // Start countdown
     let count = 5

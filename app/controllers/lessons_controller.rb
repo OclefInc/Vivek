5623bd51fc9b46f3f9359b7516935b8ccd65133e
Phong Le <phong@oclef.com>
2025-10-28 09:00:44 -0700

    inline editing

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -20,6 +20,9 @@ class LessonsController < ApplicationController
 
   # GET /lessons/1/edit
   def edit
+    if params[:field] == "description"
+      render partial: "description_form", layout: false
+    end
   end
 
   # POST /lessons or /lessons.json

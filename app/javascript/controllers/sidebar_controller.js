f1631fb98f7c35bfcec7545f1c3a424a8c890d15
Phong Le <phong@oclef.com>
2025-10-29 12:26:30 -0700

    side menu for admin

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu", "overlay"]
+
+  toggle() {
+    if (this.menuTarget.style.transform === "translateX(0px)") {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+  open() {
+    this.menuTarget.style.transform = "translateX(0px)"
+    this.overlayTarget.classList.remove("hidden")
+  }
+
+  close() {
+    this.menuTarget.style.transform = "translateX(-100%)"
+    this.overlayTarget.classList.add("hidden")
+  }
+}

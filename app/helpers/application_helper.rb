4ac1270edfeb3cf65dcc08468e1bde6ac26218f2
Phong Le <phong@oclef.com>
2025-10-30 06:47:45 -0700

    highlight menu item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def nav_link_class(path)
+  def nav_link_class(path_array)
     base_class = "block px-5 py-3"
     active_class = "bg-sky-200 text-cyan-700 font-semibold border-l-4 border-cyan-600"
     inactive_class = "text-cyan-600 hover:bg-sky-200 hover:text-cyan-500"
 
-    if current_page?(path) || request.path.start_with?(path)
+    if path_array.any? { |path| current_page?(path) || request.path.start_with?(path) }
       "#{base_class} #{active_class}"
     else
       "#{base_class} #{inactive_class}"

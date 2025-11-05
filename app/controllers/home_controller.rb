c505d5b9caafafebb4ebccfee73eae98915c7458
Phong Le <phong@oclef.com>
2025-11-05 11:10:43 -0800

    display completed projects in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
 layout "public"
   # GET /assignments or /assignments.json
   def index
+    @projects = Assignment.joins(:summary_video_attachment)
   end
   def about
   end
   def contact
   end
-
 end

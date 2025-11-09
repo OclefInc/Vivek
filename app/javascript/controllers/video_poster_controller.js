c96f25a5c06387fe61e3016232bc1ccf094b49e8
Phong Le <phong@oclef.com>
2025-11-09 09:17:28 -0800

    poster for assignment and lesson videos

---
+++ app/javascript/controllers/video_poster_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["video", "overlay"]
+
+  connect() {
+    this.videoTarget.addEventListener('play', this.handlePlay.bind(this))
+  }
+
+  handlePlay() {
+    // Hide the overlay when video starts playing
+    this.overlayTarget.classList.add('hidden')
+  }
+}

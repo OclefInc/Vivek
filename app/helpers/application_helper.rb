59ed87518769d5c8d2527398e2070d575f06f441
Phong Le <phong@oclef.com>
2025-11-19 11:48:05 -0800

    fix admin menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,13 @@ module ApplicationHelper
     active_class = "bg-sky-200 dark:bg-gray-700 text-cyan-700 dark:text-cyan-400 font-semibold border-l-4 border-cyan-600 dark:border-cyan-500"
     inactive_class = "text-cyan-600 dark:text-cyan-400 hover:bg-sky-200 dark:hover:bg-gray-700 hover:text-cyan-500 dark:hover:text-cyan-300"
 
-    if path_array.any? { |path| current_page?(path) || request.path.start_with?(path) }
+    if path_array.any? { |path|
+      if path == "/admin" || path == "/admin/"
+        request.path == "/admin" || request.path == "/admin/"
+      else
+        current_page?(path) || request.path.start_with?(path)
+      end
+    }
       "#{base_class} #{active_class}"
     else
       "#{base_class} #{inactive_class}"

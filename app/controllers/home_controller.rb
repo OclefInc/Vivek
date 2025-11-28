39a33e77c867c051fdb3cadbf37ec37cfa3d0044
Phong Le <phong@oclef.com>
2025-11-28 12:42:33 -0800

    display in progress projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,7 @@ class HomeController < ApplicationController
   layout "public"
   # GET /assignments or /assignments.json
   def index
-    @projects = Assignment.joins(:summary_video_attachment)
-
-    respond_to do |format|
-      format.html
-      format.any { render html: "", layout: false }
-    end
+    @projects = Assignment.all
   end
   def about
   end

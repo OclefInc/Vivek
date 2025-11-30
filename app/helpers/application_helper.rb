797a8778e09fd5bfb26932f193495890372ee2b1
Phong Le <phong@oclef.com>
2025-11-30 11:50:51 -0800

    show music theory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,18 +45,26 @@ module ApplicationHelper
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
       </svg>'
     when "Music Theory"
-      # Light Bulb
-      raw '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-4.5 6.01 6.01 0 00-1.5-4.5m0 9c-.4 0-.777.047-1.133.138m1.133-.138c.356.091.733.138 1.133.138m-2.266-9A6.008 6.008 0 017.5 3.75c0 .36.028.711.082 1.055.054.345.14.674.254.985.115.31.262.602.437.872l.001.002.001.002A6.01 6.01 0 0112 9.75m-6 0a6.01 6.01 0 001.5 4.5 6.01 6.01 0 00-1.5-4.5m0 9c.4 0 .777.047 1.133.138M6 18v-5.25m0 0c.356.091.733.138 1.133.138M18 18v-5.25m0 0a6.01 6.01 0 01-1.5-4.5 6.01 6.01 0 011.5-4.5m0 9c.4 0 .777.047 1.133.138m-1.133-.138c-.356.091-.733.138-1.133.138" />
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-      </svg>'
+      # Treble Clef
+      file_path = Rails.root.join("app", "assets", "images", "rickvanderzwet_Treble_clef_1.svg")
+      if File.exist?(file_path)
+        svg = File.read(file_path)
+        # Make it colorable and size it
+        svg.sub!(/<svg/, '<svg class="w-8 h-8"')
+        svg.gsub!(/stroke="#000"/, 'stroke="currentColor" fill="currentColor"')
+        raw svg
+      else
+        # Fallback if file not found
+        raw '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 16.5a3 3 0 1 0 6 0 3 3 0 0 0-6 0m3-13.5v13.5m0-13.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5m0 4.5a4.5 4.5 0 1 0 0 9 4.5 4.5 0 0 0 0-9" />
+        </svg>'
+      end
     when "Foundation Development"
       # Cube
       raw '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
       </svg>'
     else
-      # Default: Document
       raw '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
       </svg>'

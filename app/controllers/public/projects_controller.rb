0a6cdf33826a5a663683a82033532288cab4267f
Phong Le <phong@oclef.com>
2025-11-30 21:00:28 -0800

    search functionality

--- app/controllers/public/projects_controller.rb
+++ app/controllers/public/projects_controller.rb
@@ -2,6 +2,9 @@ class Public::ProjectsController < ApplicationController
   layout "public"
   def index
     @projects = Assignment.all
+    if params[:query].present?
+      @projects = @projects.joins(:student).where("project_name ILIKE ? OR students.name ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    end
   end
 
   def show

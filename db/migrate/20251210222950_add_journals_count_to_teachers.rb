2851f3ea188c1434db073630c489658cdd223c6a
Phong Le <phong@oclef.com>
2025-12-12 18:40:47 -0800

    fix migration

--- db/migrate/20251210222950_add_journals_count_to_teachers.rb
+++ db/migrate/20251210222950_add_journals_count_to_teachers.rb
@@ -3,14 +3,14 @@ class AddJournalsCountToTeachers < ActiveRecord::Migration[8.0]
     add_column :teachers, :journals_count, :integer, default: 0, null: false
 
     # Initialize counter cache for existing records
-reversible do |dir|
+    reversible do |dir|
       dir.up do
-        Teacher.find_each do |te acher|
-          if t eacher.user.present?
-            journals_count = te a cher.user.journals.count
-            teacher.update_column(:jou rnals_count, journals_count)
+        Teacher.find_each do |teacher|
+          if teacher.user.present?
+            journals_count = teacher.user.journals.count
+            teacher.update_column(:journals_count, journals_count)
           end
-                      end
+        end
       end
     end
   end
